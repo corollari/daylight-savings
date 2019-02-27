126c5a6cb524b5c6492c26f848b3babbf93a121e
Albert <github@albert.sh>
2019-02-27 23:14:41 +0100

    Adapt plotting to the refactored simulation code

--- plotRadiation.py
+++ plotRadiation.py
@@ -4,6 +4,6 @@ from simulation import *
 DAY=1
 
 time=np.arange(0,24*60)
-radi=list(map(lambda x:solarRadiation(x, DAY, LATITUDE), time))
-plt.plot(time, radi, 'o')
+radiation=list(map(lambda x:solarRadiation(x, DAY, 40), time))
+plt.plot(time, radiation, 'o')
 plt.show()
